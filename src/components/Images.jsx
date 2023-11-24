698058a7161e473803dd4de4b7a7f2bae53a54c1
NashibJr <nashibjr@gmail.com>
2023-11-24 11:47:48 -0800

    I have added the carousel and the switching of the active links

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 import miami from "../assets/images/miami.jpg";
+import pretoria from "../assets/images/pretoria.jpg";
+import rio from "../assets/images/rio.jpg";
 
 const Images = () => {
+  const images = [miami, pretoria, rio];
+  const [pages, setPages] = React.useState(1);
+
+  let paginations = [];
+  const myimage = images[pages - 1];
+  for (let index = 0; index < images.length; index++) {
+    paginations = [...paginations, index + 1];
+  }
+
+  React.useEffect(() => {
+    if (pages >= 4) {
+      setPages(1);
+    }
+    const interval = window.setInterval(() => {
+      setPages(pages + 1);
+    }, 5000);
+    return () => window.clearInterval(interval);
+  }, [pages]);
+
   return (
     <div
-      className="w-full h-[250px] mt-2 rounded-lg flex justify-center flex-col items-center md:h-[350px]"
+      className="w-full h-[250px] mt-2 rounded-lg flex justify-center flex-col items-center md:h-[350px] myanimation pt-10"
       style={{
-        backgroundImage: `url(${miami})`,
+        backgroundImage: `url(${myimage})`,
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center",
@@ -29,6 +50,16 @@ const Images = () => {
       </p>
       <div className="w-6 h-6 bg-red-600 rounded-full text-center text-white m-2">
         1
+      </div>
+      <div className="flex mt-1">
+        {paginations.map((pagination, index) => (
+          <div
+            className={`w-4 h-4 rounded-full bg-white ${
+              index + 1 === pages ? "bg-opacity-80" : "bg-opacity-40"
+            } m-2`}
+            key={index}
+          ></div>
+        ))}
       </div>
     </div>
   );
